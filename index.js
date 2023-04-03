867b42a0002e18dd1e2a9971b68fb350562711f2
yuri <yuri@example.com>
2023-04-03 18:51:47 +0900

    9. 指定したPORT番号でサーバーが立ち上がるのか確認してみよう

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+app.get("/", (req, res) => {
+  res.send("ハロー✨");
+});
+
 app.listen(PORT, () => {
   console.log("ローカルサーバー起動中🚀");
 });
