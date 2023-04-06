88358fc7756c0ccab98b65b648703dc30df8ae56
yuri <yuri@example.com>
2023-04-06 19:47:00 +0900

    33. JWTをデコード(複合)する関数を作成しよう

--- src/v1/handlers/tokenHandler.js
+++ src/v1/handlers/tokenHandler.js
@@ -1,6 +1,21 @@
+const JWT = require("jsonwebtoken");
+require("dotenv").config();
+
 //クライアントから渡されたJWTが正常か認証
+const tokenDecode = (req) => {
+  const bearerHeader = req.headers["authorization"];
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(" ")[1];
+    try {
+      const decodedToken = JWT.verify(bearer, process.env.TOKEN_SECRET_KEY);
+      return decodedToken;
+    } catch (err) {
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
 
 //JWT認証を検証するためのミドルウェア
-exports.verifyToken = (req, res, next) => {
-
-}
+exports.verifyToken = (req, res, next) => {};
